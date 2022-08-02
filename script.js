75ea09432e81cf8be9e5845aaff479f268eea03f
nononon <103585529+nononon00@users.noreply.github.com>
2022-08-03 06:13:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -103,3 +103,20 @@ function handleSSS() {
     transactionHttp.announce(signedTx)
   })
 }
+
+
+
+function CopyToClipboard(containerid) {
+if (document.selection) { 
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand("copy"); 
+
+} else if (window.getSelection) {
+    var range = document.createRange();
+     range.selectNode(document.getElementById(containerid));
+     window.getSelection().addRange(range);
+     document.execCommand("copy");
+     alert("Address Copied") 
+}}
