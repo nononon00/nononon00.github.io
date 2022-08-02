8e4f56f8587a7649c8180d285ac76c3c8698aa28
nononon <103585529+nononon00@users.noreply.github.com>
2022-08-03 06:23:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -105,18 +105,3 @@ function handleSSS() {
 }
 
 
-
-function CopyToClipboard(containerid) {
-if (document.selection) { 
-    var range = document.body.createTextRange();
-    range.moveToElementText(document.getElementById(containerid));
-    range.select().createTextRange();
-    document.execCommand("copy"); 
-
-} else if (window.getSelection) {
-    var range = document.createRange();
-     range.selectNode(document.getElementById(containerid));
-     window.getSelection().addRange(range);
-     document.execCommand("copy");
-     alert("Address Copied") 
-}}
