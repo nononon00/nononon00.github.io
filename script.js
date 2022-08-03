c4995c178ef9cb462151c6f447ce235a311f66c4
nononon <103585529+nononon00@users.noreply.github.com>
2022-08-04 06:19:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ setTimeout(() => {
 const address = symbol.Address.createFromRawAddress(window.SSS.activeAddress)
 
 const dom_addr = document.getElementById('wallet-addr')
-dom_addr.innerText = address.pretty()
+dom_addr.innerText = address()
 
 accountHttp.getAccountInfo(address)
   .toPromise()
